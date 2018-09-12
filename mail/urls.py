f693bb1376b8bd67f90c8deba8414b775bc46450
Dorgivan <dorgivansouza@gmail.com>
2018-09-12 10:09:18 -0300

    foi mal

---
+++ mail/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
+
+from . import views
+
+urlpatterns = [
+
+]
